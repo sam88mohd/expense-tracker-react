b47bb592e175c233259bc3e14dd359cef0205cdd
Samsudin Mohamad <sam88mohd@gmail.com>
2022-03-20 15:43:38 +0800

    create income expense component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Balance from "./components/Balance";
 import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </>
   );
