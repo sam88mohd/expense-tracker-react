d1106745a4a49759c2e3f13399e90d19405efd9e
Samsudin Mohamad <sam88mohd@gmail.com>
2022-03-20 22:31:35 +0800

    create incomeExpense component

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const IncomeExpenses = () => {
-  return <div>IncomeExpenses</div>;
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">+ RM 0.00</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">- RM 0.00</p>
+      </div>
+    </div>
+  );
 };
 
 export default IncomeExpenses;
