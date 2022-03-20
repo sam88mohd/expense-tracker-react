47965b61ab995bc3643424c54d3e23dd2580edef
Samsudin Mohamad <sam88mohd@gmail.com>
2022-03-20 15:38:26 +0800

    create balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Balance from "./components/Balance";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </>
   );
 };
